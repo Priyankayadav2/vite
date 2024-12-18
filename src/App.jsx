105f8865dbea03c4eafccc5a01dc2d21eb95777c
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2024-12-18 16:11:38 +0530

    difference between variable and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react"
-
 function App() {
 const [data,setData]=useState("peter")
 function update(){
   setData('john')
-}
-
-
+} 
+function updateEmail(){
+  emailId="anil@gmail.com"
+  console.log(emailId);
+  
+} 
+  let emailId="test@gmail"
+  emailId="demo@gmail.com"
   return (
   <div>
       <h1>State</h1>
       <h2>{data}</h2>
+      <h3>{emailId}</h3>
       <button onClick={update}>Update Data</button>
+      <button onClick={updateEmail}>Update Email</button>
     </div>
   )
 }
