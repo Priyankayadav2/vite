90e63a21bab337ed3303b87d45dfbbc83161e513
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2024-12-20 16:29:42 +0530

    component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import { useState } from "react"
+import Teacher,{User} from './User'
+
 function App() {
-const [count,setCount]=useState(0)
-function countFun(item){
-  if(item==="increment"){
-    setCount(count+1)
-  }
-  else if(item==="decrement"){
-    setCount(count-1)
-  }
-}
   return (
   <div>
-    <button onClick={()=>countFun("decrement")}>decrement</button>
-    <h2>Count = {count}</h2>
-    
-    <button onClick={()=>countFun("increment")}>increment</button>
+    <h1>Components</h1>
+    <User/>
+    <Teacher/>
     </div>
   )
 }
 export default App
+
+
---
+++ src/User.jsx
@@ -0,0 +1,15 @@
+export function User(){
+return(
+    <div>
+    <h1>User</h1>
+    </div>
+)
+}
+function Student(){
+    return(
+        <div>
+        <h1>Student</h1>
+        </div>
+    )
+    }
+    export default Student
