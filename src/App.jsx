1a93d361b80380e1ba873cbbd14303937130d6cf
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2024-12-24 16:18:03 +0530

    use ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
 function App() {
-const [count,setCount]=useState(0)
-function countFun(item){
-  if(item==="increment"){
-    setCount(count+1)
-  }
-  else if(item==="decrement"){
-    setCount(count-1)
-  }
-}
-  return (
-  <div>
-    <button onClick={()=>countFun("decrement")}>decrement</button>
-    <h2>Count = {count}</h2>
+  const [count, setCount] = useState(1);
+  function countFun(item) {
+    if(count<=1 && item==="decrement"){
+      return false;
+    }
+    // setCount((preCount) => 
+    //   item === "increment" ? preCount+1 : Math.max(preCount-1,1)
+    // );
+    let a=count;
+    // if(item==="increment"){
+    //   a++;
+    // }
+    // else{
+    //   a--;
+    // }
+    item ==="increment" ? a++ : a--
     
-    <button onClick={()=>countFun("increment")}>increment</button>
+    setCount(a)
+    
+  }
+  return (
+    <div>
+      <button onClick={() => countFun("decrement")}>decrement</button>
+      <h2>Count = {count}</h2>
+      <button onClick={() => countFun("increment")}>increment</button>
     </div>
-  )
+  );
 }
-export default App
+export default App;
+
