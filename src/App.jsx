ef59f7723a4418d6d3f3646b8eef95ce0860de8e
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2024-12-26 14:32:18 +0530

    destructing object

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import User from './User'
 function App() {
-  let name="rahul"
-  let email="rahul01@gmail"
+  const userdata = {
+    name:"rahul",
+    email:"rahul01@gmail"
+  };
   return (
     <div>
       <h1>Props</h1>
-      <User name={name} email={email}/>
+      <User user={userdata}/>
     </div>
   );
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,13 +1,11 @@
-function User(props){
-    console.log(props);
-    // let name = props.name
-    // let email =  props.email
-    let {name,email} =props
+function User({user}){
+    console.log(user);
+    const {name,email}=user;
     return (
         <div>
             <h1>User</h1>
-            <h2>{name}</h2>
-            <h2>{email}</h2>
+            <h2>Name : {name}</h2>
+            <h2>Email : {email}</h2>
             
         </div>
     )
