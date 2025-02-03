5966f45f71ab2622fc3d634de4f4dbd568563841
Priyankayadav2 <104354982+Priyankayadav2@users.noreply.github.com>
2025-02-03 17:01:47 +0530

    forms

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -4,19 +4,37 @@ function Signup() {
     const[email,setEmail]=useState()
     const[password,setPassword]=useState()
     const[subject,setSubject]=useState()
-
+    const[skills,setSkills]=useState()
 
     const handleNameChange = (event) => {
         setName(event.target.value);
         console.log(event);
         
     };
-    const submit=()=>{
-        console.log(name,email,password,subject)
+    const handleSkills=(event)=>{
+        console.log(event.target.value);
+        console.log(event.target.checked);
+        if(event.target.checked){
+            setSkills(event.target.value);
+            console.log("work");
+        }
+        else{
+            setSkills()
+            console.log("not work");
+        }
+            
+        }
 
+    
+    const submit=()=>{
+        console.log(name,email,password,subject,checkbox)
+    
     }
     return(
         <div>
+            <h1>
+                {skills}
+            </h1>
             <h1>Sign Up Page</h1>
             <div>
             <label for="fname">Enter name: </label>
@@ -28,12 +46,48 @@ function Signup() {
             <label for="password">Password : </label>
             <input type="password" onChange={(event)=>{setPassword(event.target.value)}} placeholder="password"/><br /><br />
             
-            <label for="subject">Choose a Subject:</label>
+            
+            <label for="container">Skills : </label>
+            
+            <input type="checkbox"  value="javascript" onChange={(event)=>{handleSkills(event)} }/>
+            
+            <label class="container">JavaScript
+    
+            
+            <span class="checkmark"></span>
+            </label>
+            
+            <input type="checkbox" value="python" onChange={(event)=>{handleSkills(event)} }/>
+    
+            
+            <label class="container">Python
+
+            <span class="checkmark"></span>
+            </label>
+
+            <input type="checkbox"  value="reactjs" onChange={(event)=>{handleSkills(event)} }/>
+    
+            
+            <label class="container">ReactJs
+            
+            <span class="checkmark"></span>
+            </label><br /><br/>
+            <label for="subject">Choose a Subject : </label>
 
             <select name="subject" onChange={(event)=>{setSubject(event.target.value)}} id="subjects">
             <option value="math">Maths</option>
             <option value="Science">Science</option>
+            <option value="English">English</option>
+            <option value="Social Science">Social Science</option>
             </select>
+            <br /><br />
+            <label for="gender">Gender : </label>
+            <input type="radio" name="gender" />
+            <label for="html">Male</label>
+            <input type="radio"  name="gender"/>
+            <label for="css">Female</label>
+            <input type="radio"  name="gender" />
+            <label for="javascript">Other</label><br /><br />
             <button onClick={submit}>Submit</button><br />
             </div>
 
